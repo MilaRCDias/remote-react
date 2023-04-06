e466b639bee3b8fad1869dbaf95c146ff46113b5
MilaRCDias <MilaRCDias@users.noreply.github.com>
2023-04-06 09:20:03 +0200

    Remote react webpack

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <section style={{border: '1px dashed blue', padding: '20px'}}>
+    
+        <p style={{color: 'blue'}}>Remote React webpack</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
